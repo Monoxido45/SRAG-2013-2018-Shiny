fcfe720c73657b0ab0dced070331eff6c874a9e5
Monoxido45 <lucruz45.cab@gmail.com>
2022-04-13 12:21:26 -0300

    3 rds adicionados na pasta analise

--- data-raw/limpeza_dados.R
+++ data-raw/limpeza_dados.R
@@ -51,18 +51,69 @@ dados_SRAG <- dados_SRAG |>
 
 
 # dando join para extrair nome de municipios, UF e mesorregiao
-dados_SRAG <- dados_SRAG |> left_join(ibge_muni |> select(Nome_UF,"Mesorregião Geográfica",
-                                                        "Nome_Mesorregião",
-                                                        "Região Geográfica Imediata",
-                                                        "Nome Região Geográfica Imediata",
+dados_SRAG <- dados_SRAG |> left_join(ibge_muni |> select(Nome_UF,
+                                                        "Região Geográfica Intermediária",
                                                         "Código Município Completo",
                                                         "Nome_Município") |>
                                       rename("ID_MUNICIP" = "Código Município Completo") |>
                                       mutate(ID_MUNICIP = ID_MUNICIP |> as.character() |> 
                                                substring(1, 6) |> as.numeric()),
-                                      by = "ID_MUNICIP")
+                                      by = "ID_MUNICIP") |>
+  left_join(abjData::pnud_min |> filter(ano == "2010") |>
+              rename(ID_MUNICIP = muni_id) |>
+              mutate(ID_MUNICIP = ID_MUNICIP |> as.character() |> 
+                       substring(1, 6) |> as.numeric()) |>
+              select(ID_MUNICIP, regiao_nm, pop, lat, lon),
+            by = "ID_MUNICIP") |>
+  mutate(regiao_nm = case_when(regiao_nm == "Norte" ~ 1,
+                   regiao_nm == "Nordeste" ~ 2,
+                   regiao_nm == "Sudeste" ~ 3,
+                   regiao_nm == "Sul" ~ 4,
+                   regiao_nm == "Centro-Oeste" ~ 5)) |>
+  rename(MesoRegion = "Região Geográfica Intermediária",
+         State = SG_UF_NOT,
+         Region = regiao_nm)
 
 
 dados_SRAG |> write_rds("analise/dados_SRAG.rds")
+
+# obtendo estatisticas associadas as pop em 2010
+estat_muni_comp <- abjData::pnud_min |> filter(ano == "2010") |>
+  select(uf_sigla, regiao_nm, pop, muni_id) |>
+  mutate(muni_id = muni_id |> as.numeric(),
+        regiao_nm = case_when(regiao_nm == "Norte" ~ 1,
+                               regiao_nm == "Nordeste" ~ 2,
+                               regiao_nm == "Sudeste" ~ 3,
+                               regiao_nm == "Sul" ~ 4,
+                               regiao_nm == "Centro-Oeste" ~ 5)) |>
+left_join(ibge_muni |>  select(UF,
+                               "Região Geográfica Intermediária",
+                               "Código Município Completo",
+                               "Nome_Município") |>
+            rename("muni_id" = "Código Município Completo"),
+          by = "muni_id") |>
+  rename(MesoRegion = "Região Geográfica Intermediária",
+         State = UF,
+         Region = regiao_nm)
+
+# calculando pops
+pop_reg <- estat_muni_comp |> 
+  group_by(Region) |>
+  summarise(pop = sum(pop))
+
+# salvando
+pop_reg |> write_rds("analise/pop_Region.rds")
+
+# calculando para os estados
+pop_uf <- estat_muni_comp |> 
+  group_by(State) |>
+  summarise(pop = sum(pop))
+pop_uf |> write_rds("analise/pop_State.rds")
+
+# calculando para as macorregios
+pop_meso <- estat_muni_comp |> 
+  group_by(MesoRegion) |>
+  summarise(pop = sum(pop))
+pop_meso |> write_rds("analise/pop_MesoRegion.rds")
 
 
